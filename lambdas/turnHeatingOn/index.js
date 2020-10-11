24496dacfd45c8597075debdb19270409986ae3a
Kalle Bjurek <kalle.bjurek@gmail.com>
2020-10-11 14:04:04 +0200

    Deployment test

--- lambdas/turnHeatingOn/index.js
+++ lambdas/turnHeatingOn/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from MyNewShinyLambda and GithubActions!'),
+        body: JSON.stringify('Hello from MyShinyLambda and GithubActions!'),
     };
     return response;
 };
