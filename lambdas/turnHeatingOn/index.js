a42b6df09c68360f96f227c4a87eb496ba41ca1a
Kalle Bjurek <kalle.bjurek@gmail.com>
2020-10-31 17:19:33 +0100

    Push changes to shadow

--- lambdas/turnHeatingOn/index.js
+++ lambdas/turnHeatingOn/index.js
@@ -1,7 +1,22 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from MyShinyLambda and GithubActions!'),
-    };
-    return response;
-};
+const https = require('https')
+const options = {
+    hostname: 'https://ab1e6p1gfdk1g-ats.iot.eu-central-1.amazonaws.com/things/BasementRPi/shadow?name=Heater',
+    port: 443,
+    path: '/',
+    method: 'POST'
+  };
+const postData = JSON.stringify({state:{desired:{heat:true}}})
+  
+exports.handler = async function(event) {
+    const promise = new Promise((resolve, reject) => {
+        var req = https.request(options, (res) => {
+                resolve(res.statusCode)
+            });
+        req.on('error', (e) => {
+            reject(Error(e))
+        });
+        req.write(postData)
+        req.end(); 
+        })
+    return promise
+}
