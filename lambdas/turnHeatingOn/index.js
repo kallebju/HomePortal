96f293a0d23af75a7cbaee7bfc8c51ba61e28dec
Kalle Bjurek <kalle.bjurek@gmail.com>
2020-10-31 19:26:04 +0100

    Use aws sdk

--- lambdas/turnHeatingOn/index.js
+++ lambdas/turnHeatingOn/index.js
@@ -1,22 +1,30 @@
-const https = require('https')
-const options = {
-    hostname: 'https://ab1e6p1gfdk1g-ats.iot.eu-central-1.amazonaws.com/things/BasementRPi/shadow?name=Heater',
-    port: 443,
-    path: '/',
-    method: 'POST'
-  };
+var AWS = require('aws-sdk');
+
+var iotdata = new AWS.IotData({
+    endpoint:"ab1e6p1gfdk1g-ats.iot.eu-central-1.amazonaws.com",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
 const postData = JSON.stringify({state:{desired:{heat:true}}})
+var params = {
+    payload: postData,
+    thingName: 'BasementRPi',
+    shadowName: 'Heater'
+};
   
 exports.handler = async function(event) {
     const promise = new Promise((resolve, reject) => {
-        var req = https.request(options, (res) => {
-                resolve(res.statusCode)
-            });
-        req.on('error', (e) => {
-            reject(Error(e))
-        });
-        req.write(postData)
-        req.end(); 
+        iotdata.updateThingShadow(params, (err,data) => {
+            if (err){
+                console.log('An error has occured: ',err.errorMessage)
+                reject(err)
+            }
+            else{
+                console.log(`Response from updateThingShadow: ${JSON.stringify(data)}`)
+                resolve(data)
+            }     
         })
+        });
     return promise
 }
